13b3848d521b081a0c702141f77aff6d69fd064c
Dinindu98 <dinindu.ayagama@gmail.com>
2020-07-06 18:49:57 +0530

    improved UI

--- app/src/main/java/com/hrandika/angular/onedu/assignment_submission/Assignment_Submission.kt
+++ app/src/main/java/com/hrandika/angular/onedu/assignment_submission/Assignment_Submission.kt
@@ -1,4 +1,4 @@
-package com.hrandika.angular.onedu
+package com.hrandika.angular.onedu.assignment_submission
 
 import android.content.Context
 import android.net.Uri
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hrandika.angular.onedu.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
