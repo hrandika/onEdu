7316ebef9f7d143bb128a10c8a5ea4a045917a18
udith-shalinda <udithshalinda2@gmail.com>
2020-08-09 18:20:48 +0530

    home page updated

--- app/src/main/java/com/hrandika/angular/onedu/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hrandika/angular/onedu/ui/home/HomeFragment.kt
@@ -22,10 +22,10 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
