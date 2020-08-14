03f97a77e9103875c4fbbb67ab469b6584c26404
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-08-15 00:01:35 +0530

    SE-2016-017(K.M.S.Karunarathna)

--- app/src/main/java/com/hrandika/angular/onedu/utils/crypto/BiometricPromptUtils.kt
+++ app/src/main/java/com/hrandika/angular/onedu/utils/crypto/BiometricPromptUtils.kt
@@ -39,6 +39,35 @@ object BiometricPromptUtils {
         return BiometricPrompt(activity, executor, callback)
     }
 
+    fun createBiometricPromptForAutoLogin(
+        activity: AppCompatActivity,
+        processSuccess: (BiometricPrompt.AuthenticationResult) -> Unit
+    ): BiometricPrompt {
+        val executor = ContextCompat.getMainExecutor(activity)
+
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+
+            override fun onAuthenticationError(errCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errCode, errString)
+                Log.d(TAG, "errCode is $errCode and errString is: $errString")
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Log.d(TAG, "Biometric authentication failed for unknown reason.")
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Log.d(TAG, "Authentication was successful")
+                processSuccess(result)
+            }
+        }
+        return BiometricPrompt(activity, executor, callback)
+    }
+
+
+
     fun createPromptInfo(activity: AppCompatActivity): BiometricPrompt.PromptInfo =
         BiometricPrompt.PromptInfo.Builder().apply {
             setTitle(activity.getString(R.string.prompt_info_title))
